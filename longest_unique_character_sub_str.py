2623a575df195cf7be586f76498c4868cb2e023f
anulata1234 <39918166+anulata1234@users.noreply.github.com>
2019-10-11 07:19:12 +0530

    Update longest_unique_character_sub_str.py

--- longest_unique_character_sub_str.py
+++ longest_unique_character_sub_str.py
@@ -1,26 +1,13 @@
 def lengthOfLongestSubstring(s):
-    """
-    :type s: str
-    :rtype: int
-    """
-
-    curSubStr = ""
-    curSubStrDict = {}
-    maxSubStr = ""
-    for ch in s :
-        if ch in curSubStrDict :
-            if len(maxSubStr) < len(curSubStr):
-                maxSubStr = curSubStr
-            curSubStrDict = {}
-            curSubStrDict[ch] = ch
-            curSubStr = ""+ch
-
-        else :
-            curSubStrDict[ch] = ch
-            curSubStr += ch
-
-    return len(curSubStr) if len(curSubStr) > len(maxSubStr) else len(maxSubStr)
-
-x = lengthOfLongestSubstring("abbbba")
-x = lengthOfLongestSubstring("xyzx")
-print(x)
+        if s == "":
+            return 0
+        MaxLength = 0
+        TempString = ""
+        for i in s:
+            if i not in TempString:
+                TempString += i
+                if len(TempString) > MaxLength:
+                    MaxLength = len(TempString)
+            else:
+                TempString = TempString[TempString.index(i)+1:] + i
+        return MaxLength
